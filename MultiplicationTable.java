c06f72101cdfd06cc0cf3413bec2a07bedbb8f9a
saood <saood.rahman7@gmail.com>
2021-04-03 19:58:47 +0500

    3rd commit

---
+++ MultiplicationTable.java
@@ -0,0 +1,10 @@
+class MultiplicationTable{
+    public static void main(String[] args){
+        for(int i = 1; i <= 12; i++){
+            for(int j = 1; j <= 12; j++){
+                int k = i * j;
+                System.out.println(i + " * " + j + " = " + k);
+            }
+        }
+    }
+}
